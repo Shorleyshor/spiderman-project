39ebc8c418e8cef5ef90552bfb3bbfbd199bdeb2
shola Daropale <sholadennis@yahoo.com>
2022-05-12 02:19:16 +0200

    event handler connected to a fully styled web page

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,22 @@
-console.log("hi from index")
+//target start button//
+const startButton = document.getElementById("start-btn");
+
+//target the banner section//
+const bannerSection = document.getElementById("banner");
+
+//function to render question to page//
+const renderQuestion = () => {
+    console.log("render question")
+}
+//declare event handler function for start button click//
+const handleStartButtonClick = () => {
+    console.log("start button click")
+
+//remove banner section//
+
+//render question//
+renderQuestion();
+};
+
+//add event listener to start button//
+startButton.addEventListener("click", handleStartButtonClick);
